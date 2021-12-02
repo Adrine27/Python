18d3f0e4c448c7231808617fde9463d8a3558763
Adrine27 <91273930+Adrine27@users.noreply.github.com>
2021-12-02 22:47:37 +0400

    Add files via upload

---
+++ file_handling.py
@@ -0,0 +1,59 @@
+'''1.Create 5 file (.txt) and write messages in 
+them.'''
+# f1 = open('file1.txt','w')
+# text1 = 'abc'
+# f1.write(text)
+
+# f2 = open('file2.txt','w')
+# text2 = 'def'
+# f2.write(text2)
+
+# f3 = open('file3.txt','w')
+# text3 = 'ghi'
+# f3.write(text3)
+
+# f4 = open('file4.txt','w')
+# text4 = 'jkl'
+# f4.write(text4)
+
+# f5 = open('file5.txt','w')
+# text5 = 'mno'
+# f5.write(text5)
+
+'''2. Write a Python program to read first n lines of
+a file.'''
+# f = open('file.txt','w')
+# text = 'Hello\nI am Adrine\nI am 18 years old'
+# f.write(text)
+# f = open('file.txt','r')
+# lines = f.readlines()
+# first_lines = lines[:2]
+# print(first_lines)
+
+'''3.Write a Python program to append text to a file and
+display the text.'''
+# f = open('file.txt','a')
+# f.write('\nI am programmer')
+
+'''4.Write a python program to find the longest words.'''
+# f = open('file.txt','r')
+# for i in f:
+# 	print(len(i))
+
+'''5.Write a python program to find numbers in txt.'''
+# f = open('file.txt','r')
+# lines = f.readlines()
+# for i in lines:
+# 	for j in i:
+# 		if j.isdigit():
+# 			print(j)
+
+'''6.Write a python program to get login and password'''
+# with open('logpass.txt','w',encoding = 'utf-8') as f:
+# 	login = input('Login: ')
+# 	password = input('password: ')
+# 	my_str = login + '\n' + password
+# 	f.write(my_str)
+	
+
+
