c85a6fb74b44e1e8926b21b79f99725e38ba56fc
Adrine27 <91273930+Adrine27@users.noreply.github.com>
2021-12-02 22:35:09 +0400

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,1 +1,43 @@
-tuple.py
+'''1. Write a Python program to count the
+ elements in a list until an element is a 
+ tuple.'''
+# x = (10,20,30,40,(50,60),70,80)
+# count = 0
+# for i in x:
+# 	if isinstance(i,tuple):
+# 		break
+# 	count += 1
+# print(count)
+
+'''2. Write a Python program to reverse a
+tuple.'''
+# x = (10,20,30,40)
+# y = reversed(x)
+# print(tuple(y))
+
+'''3. Write a Python program to find the
+length of a tuple.'''
+# thistuple = tuple('abcdefg')
+# print(len(thistuple))
+
+'''4. Write a Python program to convert a tuple to a string.'''
+# thistuple = ('h','e','l','l','o')
+# y = ''.join(thistuple)
+# print(y)
+
+'''5. Write a Python program which calculate the count of item in
+tuple for example: my_tuple = (1,12,15) output:28.'''
+# my_tuple = (1,12,15)
+# count = 0
+# for i in my_tuple:
+# 	i = int(i)
+# 	count += i
+# print(count)
+
+'''6. Write a Python program to find name in tuple'''
+# name = input('write your name:')
+# thistuple = ('Adrine','Nver','Geghamik','Valodik','Vle','Baghdagyul')
+# if name in thistuple:
+# 	print('There is your name')
+# else:
+# 	print("There isn't your name")
