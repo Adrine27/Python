7e4540f05cfe8cbeb0c4147abe89dfd8643d18ad
Adrine27 <91273930+Adrine27@users.noreply.github.com>
2021-12-02 22:47:48 +0400

    Add files via upload

---
+++ json.py
@@ -0,0 +1,35 @@
+'''1. Write a Python program to add info in JSON file
+about you.'''
+# import json
+# file_name = 'about_me.json'
+# me = {
+# 	'name':'Adrine',
+# 	'age':18,
+# 	'university':'NPUA'
+# }
+# my_list = [me]
+# with open(file_name, 'w') as file:
+# 	json.dump(my_list, file)
+
+'''2. Write a Python program to get info in JSON file
+about you.'''
+# import json
+# file_name = 'about_me.json'
+# file = open(file_name)
+# json_data = json.load(file)
+# info = input('what info do you want(name,age,university)? ')
+# for data in json_data:
+# 	print(data[info])
+
+'''3. Write a Python program to check if your json file
+have this info.'''
+# import json
+# file_name = 'about_me.json'
+# file = open(file_name)
+# json_data = json.load(file)
+# info = input('info(name,age,university): ')
+# for data in json_data:
+# 	if info in data:
+# 		print('yes')
+# 	else:
+# 		print('no')
