239696c889e7fc6be619008e1c44f92bb88b2d96
Adrine27 <91273930+Adrine27@users.noreply.github.com>
2021-12-02 22:40:44 +0400

    Add files via upload

---
+++ set_frozenset.py
@@ -0,0 +1,65 @@
+'''1. Write a Python program to create a set.'''
+#this_set = {11,12,13,'yes','no'}
+
+'''2. Write a Python program to iteration over sets.'''
+# this_set = {'a','b','c','a','k','c'}
+# print(set(this_set))
+
+'''3. Write a Python program to add member(s) in a set.'''
+# this_set = {11,12,13,'yes','no'}
+# this_set.add(14)
+# print(this_set)
+
+'''4. Write a Python program to remove item(s) from set'''
+# this_set = {11,12,13,'yes','no'}
+# this_set.remove(11)
+# print(this_set)
+
+'''5. Write a Python program to remove an item from a set
+if it is present in the set.'''
+# this_set = {11,12,13,'yes','no'}
+# this_set.discard(14)
+
+# this_set = {11,12,13,'yes','no'}
+# x = 11
+# if x in this_set:
+#     this_set.remove(x)
+#     print(this_set)
+# else:
+#     this_set.discard(x)
+#     print(this_set)
+
+'''6. Write a Python program to create an intersection of
+sets.'''
+# set1 = {1,2,3}
+# set2 = {4,5,1}
+# print(set1.intersection(set2))
+
+'''7. Write a Python program to create a union of sets.'''
+# set1 = {1,2,3}
+# set2 = {4,5,6}
+# print(set1.union(set2))
+
+'''8. Write a Python program to issubset and issuperset.'''
+# set1 = {1,2,3}
+# set2 = {4,5,1,2,3}
+# print(set1.issubset(set2))
+# print(set1.issuperset(set2))
+
+'''9. Write a Python program to clear a set.'''
+# this_set = {11,12,13,'yes','no'}
+# this_set.clear()
+# print(this_set)
+
+'''10. Write a Python program to find maximum and the
+minimum value in a set.'''
+# set1 = {4,5,1,2,3}
+# x = max(set1)
+# print(x)
+# y = min(set1)
+# print(y)
+
+'''11. Write a Python program to find the length of a set'''
+# set1 = {4,5,1,2,3}
+# print(len(set1))
+
